44f901632972ace83610756a544700a8a6df6a80
telgerot <edward.k.robe@gmail.com>
2018-12-22 09:04:00 +0200

    Added player ship, music player, splash screen

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] float levelTransitionTime = 3f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        StartCoroutine(LoadFirstLevel());
+    }
+
+    IEnumerator LoadFirstLevel()
+    {
+        yield return new WaitForSeconds(levelTransitionTime);
+        SceneManager.LoadScene(1);
+    }
+}
